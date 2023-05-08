7f01d56beb6e3cfd2df611343f3758fdb9955636
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-05-08 14:35:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function volume_sphere() {
-    //Write your code here
-	let radius = document.querySelector("#radius");
-	let volume = 4/3(3.14*radius*radius);
-	document.querySelector("#volume").innerHtml = volume;
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+let submit = document.querySelector("#submit");
+// console.log(submit);
+function volume_sphere(e) {
+  e.preventDefault();
+  //Write your code here
+let radius = document.querySelector("#radius").value;
+console.log(radius);
+console.log(submit);
+let volume = 4/3*(3.14*radius*radius);
+document.querySelector("#volume").innerHtml = volume;
+}
+submit.addEventListener("click",volume_sphere);
