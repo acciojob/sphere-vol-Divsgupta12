c17f030c3dfcb3852b334827f69ac7c4c581d0a8
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-05-08 14:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let submit = document.querySelector("#submit");
-// console.log(submit);
 function volume_sphere(e) {
   e.preventDefault();
   //Write your code here
 let radius = document.querySelector("#radius").value;
-console.log(radius);
-console.log(submit);
 let volume = 4/3*(3.14*radius*radius);
-document.querySelector("#volume").innerHtml = volume;
+console.log(volume);
+document.querySelector("#volume").value = volume;
 }
 submit.addEventListener("click",volume_sphere);
